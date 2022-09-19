406769b65b28a623c8469e90fe9dc99672fa1f8c
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-09-19 17:20:35 +0700

    changed showed components to App2.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App2 from "./App2";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>
 );
-
