6d2aab591220b73e82651a304e9ad7710f24266e
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-09-19 17:19:54 +0700

    Added chart js

---
+++ src/components/Chart.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function Chart(props) {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "COVID-19 Data Chart",
+      },
+    },
+  };
+
+  const data = {
+    labels: [props.datas.countryRegion],
+    datasets: [
+      {
+        label: "Active",
+        data: [props.datas.active],
+        borderColor: "#f0ad4e",
+        backgroundColor: "#f0ad4e",
+      },
+      {
+        label: "Infected",
+        data: [props.datas.confirmed],
+        borderColor: "#0275d8",
+        backgroundColor: "#0275d8",
+      },
+      {
+        label: "Recovered",
+        data: [props.datas.recovered],
+        borderColor: "#5cb85c",
+        backgroundColor: "#5cb85c",
+      },
+      {
+        label: "Deaths",
+        data: [props.datas.deaths],
+        borderColor: "#d9534f",
+        backgroundColor: "#d9534f",
+      },
+    ],
+  };
+
+  console.log(props.datas);
+  return (
+    <div>
+      <Bar options={options} data={data} />
+    </div>
+  );
+}
+
+export default Chart;
