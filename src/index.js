fcae698097f1aa1f884a8974d5a0bf921e04dbe9
Rafly Pradana Putra <raflypradana31@gmail.com>
2022-09-19 19:11:18 +0700

    retry initial deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App2 from "./App2";
-import App from "./App";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
